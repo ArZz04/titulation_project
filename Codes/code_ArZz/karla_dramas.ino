1b9c914623fa26ecefe420d649c795ddc0a7951b
Juan Arvizu <arzzdeveloper04@gmail.com>
2021-08-20 05:04:33 -0500

    edition_readme.md
    I written the instruction of use of  GitHub folders

---
+++ Codes/code_ArZz/karla_dramas.ino
@@ -0,0 +1,65 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+//#include <DHT.h>
+//#include <>
+#include <MQ135.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+//DHT dht(2, DHT11);
+MQ135 gasSensor = MQ135(0);
+
+int value, co2lvl;
+
+byte N[8] = 
+{
+B00000,
+B01110,
+B01010,
+B01110,
+B00000,
+B00000,
+B00000,
+B00000,
+};
+  
+void setup()
+{
+  Serial.begin(9600);
+//9h  dht.begin();
+  lcd.init();
+  lcd.backlight();
+
+  lcd.createChar (0,N);
+}
+
+
+void loop()
+{
+  delay(1000);
+  lcd.setCursor(0, 0);
+  
+  lcd.print("A_Q:");
+  float ppm = gasSensor.getPPM();
+  lcd.print(ppm*100, 0);
+  lcd.print("ppm");
+  
+  //float h = dht.readHumidity();
+  //float t = dht.readTemperature();
+ 
+  //if (isnan(h) || isnan(t)) 
+ // {
+  //  Serial.println("Error obteniendo los datos del sensor DHT11");
+  //  return;
+ // }
+  
+  //float hic = dht.computeHeatIndex(t, h, false);
+
+ lcd.setCursor(0,1);
+ lcd.print("T:");
+ //lcd.print(t, 0);
+ lcd.write (byte (0));
+ lcd.print("C");
+ lcd.print("   H:");
+ //lcd.print(h, 1);
+ lcd.print("%");
+}
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # titulation_project
  This repository is a titulation project in CECyTEJ Tesistan
+ The code of the project is published in github for an easier access to code by teammates.
+
+ Information:
+
+ Carrer: Mechatronic 
+ Grade: 5to|6to semester
+
+Teammates of this project:
+
+1. Arvizu Juan | code_ArZz
+2. Barrios Christian | code_Barrios
+3. Garcia Eduardo | code_Garcia
+4. Ortiz Jonathan | code_Jonathan
+5. Vera Alondra | code_Vera
+
+Organitation GitHub:
+
+All Members hava a folder with name
+The test_code folder will be used to store code that will be used as a reference or that 
+will not be used, so as not to create confusion between the same partners.
+
+Materials:
+
+*Please insert the material's list, i't have this information*
