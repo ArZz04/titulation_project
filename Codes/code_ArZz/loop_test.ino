1468fe7153b405d114c9347c46f841c2485bcb56
Juan Arvizu <arzzdeveloper04@gmail.com>
2021-08-25 19:18:22 -0500

    Checker

--- Codes/code_ArZz/loop_test.ino
+++ Codes/code_ArZz/loop_test.ino
@@ -1,11 +1,10 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 //#include <DHT.h>
-//#include <>
 #include <MQ135.h>
 
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
-DHT dht(2, DHT11);
+//DHT dht(2, DHT11);
 MQ135 gasSensor = MQ135(0);
 
 int value, co2lvl;
@@ -25,7 +24,7 @@ B00000,
 void setup()
 {
   Serial.begin(9600);
-  dht.begin();
+//9h  dht.begin();
   lcd.init();
   lcd.backlight();
   lcd.createChar (0,N);
@@ -42,23 +41,23 @@ void loop()
   lcd.print(ppm*100, 0);
   lcd.print("ppm");
   
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  //float h = dht.readHumidity();
+  //float t = dht.readTemperature();
  
-  if (isnan(h) || isnan(t)) 
- {
-   Serial.println("Error obteniendo los datos del sensor DHT11");
-   return;
- }
+  //if (isnan(h) || isnan(t)) 
+ // {
+  //  Serial.println("Error obteniendo los datos del sensor DHT11");
+  //  return;
+ // }
   
-  float hic = dht.computeHeatIndex(t, h, false);
+  //float hic = dht.computeHeatIndex(t, h, false);
 
  lcd.setCursor(0,1);
  lcd.print("T:");
- lcd.print(t, 0);
+ //lcd.print(t, 0);
  lcd.write (byte (0));
  lcd.print("C");
  lcd.print("   H:");
- lcd.print(h, 1);
+ //lcd.print(h, 1);
  lcd.print("%");
 }
